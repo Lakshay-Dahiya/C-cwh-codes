60e39698f70ee4bfd5fac6d0b61d2a6a76c5fc5c
Lakshay-Dahiya <lakshaydahiya2003@gmail.com>
2023-08-06 22:29:27 +0530

    updates

--- tut12.cpp
+++ tut12.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 int main(){
+    int a = 7;
+    int *b= &a;
+
+    cout<<a<<endl;  //value of a
+    cout<<&a<<endl; //address of a
+    cout<<b<<endl;  //address of a
+    cout<<&b<<endl; //address of b
+    cout<<*b<<endl; //value of a
 
     return 0;
 }
