0bb92b13d1cd41a7a2c23cf8697b9a29cfe7b737
Lakshay-Dahiya <lakshaydahiya2003@gmail.com>
2023-09-03 23:30:05 +0530

    updates

---
+++ tut4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int glo = 9;
+void sum(){
+    int a;
+    cout<<glo<<endl;
+}
+int main(){
+    int glo = 5;
+    glo = 700;
+    sum();
+    cout<< glo<<endl;
+    return 0;
+}
