ad95580f32370a37e54768a9727c596943edda8e
Lakshay-Dahiya <lakshaydahiya2003@gmail.com>
2023-09-06 15:19:40 +0530

    updates

---
+++ tut7.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int c =48;
+int main(){
+    int a;
+    int b;
+    int c;
+    cout<<"enter value of a"<<endl;
+    cin>>a;
+    cout<<"enter value of b"<<endl;
+    cin>>b;
+    c = a+b;
+    cout<<"the sum is "<<c<<endl;
+    cout<<"global c is "<<::c<<endl; 
+    // Space Resolution Operator :- to use global variable in any function
+
+    //Refrence Variables
+    float x = 77;
+    float &y = x;
+    cout<<x<<endl;
+    cout<<y<<endl;
+
+
+    return 0;
+}
